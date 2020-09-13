11d7e8297ede5a0c1912f41e583c798dcd665608
David <david@Davids-MacBook-Air.local>
2020-09-13 11:34:26 +0700

    change password

--- configuration.py
+++ configuration.py
@@ -3,7 +3,7 @@ from app import app
 
 class BaseConfig:
     SECRET_KEY = "pXx3wumu67tJV58r"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Davidhuotkeo123@localhost/ticket"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:password@localhost/ticket"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     QRCODE_PASSWORD = "c028bf8b4632bbf6b691d062a5482984fea8b4d6dc2ad4913cd7082d552b"
 
